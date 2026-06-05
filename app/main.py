08e2858e10c60d3ea4931b1d8753bd62449a1e8d
Adel Muhtarov <adelmuhtarov@gmail.com>
2026-06-05 17:43:03 +0300

    4 задание

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api.router import api_router
+from app.core.config import get_settings
+from app.models.base import Base
+from app.db.session import engine
+from app.models.task import TaskORM
+from app.models.category import CategoryORM
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    from app.models.task import TaskORM
+    from app.models.category import CategoryORM
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+settings = get_settings()
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+app.include_router(api_router)
